fb31fe607d539cf7357b34e1574a9624b55a1865
Александр <alexander@MacBook-Pro-Aleksandr.local>
2018-02-19 22:18:28 +0300

    tests

--- test/HeadTailGame.test.js
+++ test/HeadTailGame.test.js
@@ -2,16 +2,63 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
 
-beforeEach( async () => {
-    accounts = await web3.eth.getAccounts();
+beforeEach(async () => {
+  	// Get a list of all accounts
+  	accounts = await web3.eth.getAccounts();
+
+  	// Use one of those accounts to deploy the contract
+  	game = await new web3.eth.Contract(JSON.parse(interface))
+    	.deploy({ data: bytecode })
+    	.send({ from: accounts[0], gas: '1000000' });
 });
 
-describe('HeadTailGame', () => {
-    it('deploy contract', () => {
-    	console.log(accounts);
+describe('Inbox', () => {
+	it('deploys a contract', () => {
+    	assert.ok(game.options.address);
+  	});
+	
+  	it('insufficient balance', () => {
+		try {
+			game.methods.enterHead().send({
+				from: accounts[1],
+				value: web3.utils.toWei('0.005', 'ether')
+			});
+			assert(false);
+		} catch(err) {
+			assert(err);
+		}
+	});
+	
+	it('player should be unique', () => {
+		game.methods.enterHead().send({
+				from: accounts[1],
+				value: web3.utils.toWei('1', 'ether')
+		});
+		try {
+			game.methods.enterHead().send({
+				from: accounts[1],
+				value: web3.utils.toWei('1', 'ether')
+			});
+			
+			assert(false);
+		} catch(err) {
+			assert(err);
+		}
+	});
+    
+    it("dealer can't play game", () => {
+        try {
+			game.methods.enterHead().send({
+				from: accounts[0],
+				value: web3.utils.toWei('1', 'ether')
+			});
+			assert(false);
+		} catch(err) {
+			assert(err);
+		}
     });
 });
-
